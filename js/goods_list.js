14c03a8d879cc4d86d088d154f9da6489a7cbcec
yhci2y93 <dhclsy85317375@163.com>
2021-04-29 14:55:22 +0800

    购物车显示功能

--- js/goods_list.js
+++ js/goods_list.js
@@ -1,11 +1,13 @@
 /**
  * Created by lenovo on 16-6-29.
  */
+if(sessionStorage.getItem("num")==null){ sessionStorage.setItem("num",0);}
 function count(){
-    if (localStorage.a) {
-        localStorage.a=Number(localStorage.a) +1;
-    } else {
-        localStorage.a=1;
-    }
-    return localStorage.a;
+    var replace=document.getElementById("shopping_cart").innerHTML;
+    replace=parseInt(replace)+1;
+    sessionStorage.setItem("num",replace);
+    document.getElementById("shopping_cart").innerHTML=replace;
 }
+var number=sessionStorage.getItem("num");
+if (isNaN(number)){number=0;}
+document.getElementById("shopping_cart").innerHTML=number;
