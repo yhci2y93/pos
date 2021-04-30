b501a5b30f99c6e1fe7db1126f93d17c40f630ad
yhci2y93 <dhclsy85317375@163.com>
2021-04-30 18:12:08 +0800

    修改了样式

--- js/addR0w.js
+++ js/addR0w.js
@@ -8,10 +8,10 @@ function add(){
     var a=[{type:'饮料',name:'可口可乐',count:'3',unit:'瓶'},{type:'饮料',name:'雪碧',count:'3',unit:'瓶'},{type:'水果',name:'苹果',count:'5.5',unit:'斤'},{type:'水果',name:'荔枝',count:'15',unit:'斤'},{type:'生活用品',name:'电池',count:'2',unit:'个'},{type:'食品',name:'方便面',count:'4.5',unit:'袋'}];
     if(replace==1){
         for(var i=0;i<a.length;i++){
-            var trObj = document.createElement("tr");
+            var trObj = document.createElement("tr");//用于创建指定的HTML元素。
             trObj.id = new Date().getTime();
             trObj.innerHTML += "<td>"+a[i].type+"</td><td>"+a[i].name+"</td><td>"+a[i].count+"</td><td>"+a[i].unit+"</td><td><button class='button_style' onclick='count()'>加入购物车</button></td>";
-            document.getElementById("tb").appendChild(trObj);
+            document.getElementById("tb").appendChild(trObj);//appendChild()方法向节点添加最后一个子节点.
         }
     }
 }
--- js/goods_list.js
+++ js/goods_list.js
@@ -1,13 +1,12 @@
 /**
  * Created by lenovo on 16-6-29.
  */
-if(sessionStorage.getItem("num")==null){ sessionStorage.setItem("num",0);}
-function count(){
-    var replace=document.getElementById("shopping_cart").innerHTML;
-    replace=parseInt(replace)+1;
-    sessionStorage.setItem("num",replace);
-    document.getElementById("shopping_cart").innerHTML=replace;
-}
+if(sessionStorage.getItem("num")==null){ sessionStorage.setItem("num",0);}//判断开始是否有数.
 var number=sessionStorage.getItem("num");
-if (isNaN(number)){number=0;}
 document.getElementById("shopping_cart").innerHTML=number;
+function count(){
+    var replace=document.getElementById("shopping_cart").innerHTML;//取出id="shopping_cart"标签之间的数，给replace.
+    replace=parseInt(replace)+1;
+    sessionStorage.setItem("num",replace);//把数据存人sessionStorage.
+    document.getElementById("shopping_cart").innerHTML=replace;//输出数到id="shopping_cart"标签之间。
+}
