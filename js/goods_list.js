d6602e378a388608f9473304f4cf3ab7f1236980
yhci2y93 <dhclsy85317375@163.com>
2021-05-05 18:47:07 +0800

    修改了函数

--- js/goods_list.js
+++ js/goods_list.js
@@ -1,12 +1,20 @@
 /**
  * Created by lenovo on 16-6-29.
  */
-if(sessionStorage.getItem("num")==null){ sessionStorage.setItem("num",0);}//判断开始是否有数.
-var number=sessionStorage.getItem("num");
-document.getElementById("shopping_cart").innerText=number;
-function count(){
-    var replace=document.getElementById("shopping_cart").innerText;//取出id="shopping_cart"标签之间的数，给replace.
-    replace=parseInt(replace)+1;
-    sessionStorage.setItem("num",replace);//把数据存人sessionStorage.
-    document.getElementById("shopping_cart").innerText=replace;//输出数到id="shopping_cart"标签之间。
+window.onload=function(){
+    shopping_check();
+    addrow();
 }
+function shopping_check(){
+    if (sessionStorage.getItem("num") == null) { sessionStorage.setItem("num", 0);}//判断开始是否有数.
+    var number = sessionStorage.getItem("num");
+    document.getElementById("shopping_cart").innerText = number;
+}
+function count(){
+    var replace = document.getElementById("shopping_cart").innerText;//取出id="shopping_cart"标签之间的数，给replace.
+    replace = parseInt(replace) + 1;
+    sessionStorage.setItem("num", replace);//把数据存人sessionStorage.
+    document.getElementById("shopping_cart").innerText = replace;//输出数到id="shopping_cart"标签之间。
+}
+
+
