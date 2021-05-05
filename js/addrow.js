92d91e30f0f2218bf3648b0cc236bedb5faa378b
yhci2y93 <dhclsy85317375@163.com>
2021-05-05 14:30:46 +0800

    修改不符合的样式还有名字

---
+++ js/addrow.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lenovo on 16-7-1.
+ */
+var replace=0;
+addrow();
+function addrow(){
+     replace=parseInt(replace)+1;
+    var goods_info=[{type:'饮料',name:'可口可乐',count:'3',unit:'瓶'},{type:'饮料',name:'雪碧',count:'3',unit:'瓶'},{type:'水果',name:'苹果',count:'5.5',unit:'斤'},{type:'水果',name:'荔枝',count:'15',unit:'斤'},{type:'生活用品',name:'电池',count:'2',unit:'个'},{type:'食品',name:'方便面',count:'4.5',unit:'袋'}];
+    if(replace==1){
+        for(var i=0;i<goods_info.length;i++){
+            var trObj = document.createElement("tr");//用于创建指定的HTML元素。
+            trObj.id = new Date().getTime();
+            trObj.innerHTML += "<td>"+goods_info[i].type+"</td><td>"+goods_info[i].name+"</td><td>"+goods_info[i].count+"</td><td>"+goods_info[i].unit+"</td><td><button class='button_style' onclick='count()'>加入购物车</button></td>";
+            document.getElementById("tb").appendChild(trObj);//appendChild()方法向节点添加最后一个子节点.
+        }
+    }
+}
--- js/goods_list.js
+++ js/goods_list.js
@@ -3,10 +3,10 @@
  */
 if(sessionStorage.getItem("num")==null){ sessionStorage.setItem("num",0);}//判断开始是否有数.
 var number=sessionStorage.getItem("num");
-document.getElementById("shopping_cart").innerHTML=number;
+document.getElementById("shopping_cart").innerText=number;
 function count(){
-    var replace=document.getElementById("shopping_cart").innerHTML;//取出id="shopping_cart"标签之间的数，给replace.
+    var replace=document.getElementById("shopping_cart").innerText;//取出id="shopping_cart"标签之间的数，给replace.
     replace=parseInt(replace)+1;
     sessionStorage.setItem("num",replace);//把数据存人sessionStorage.
-    document.getElementById("shopping_cart").innerHTML=replace;//输出数到id="shopping_cart"标签之间。
+    document.getElementById("shopping_cart").innerText=replace;//输出数到id="shopping_cart"标签之间。
 }
