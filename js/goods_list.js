16b28762e138c60c205356390c52fc984cfd7df2
yhci2y93 <dhclsy85317375@163.com>
2021-05-06 17:46:05 +0800

    修改了各种函数

--- js/goods_list.js
+++ js/goods_list.js
@@ -2,37 +2,49 @@
  * Created by lenovo on 16-6-29.
  */
 window.onload=function(){
-    shopping_check();
-    addrow();
-    goods_list_jump();
+    shopping_cart_null_check();
+    add_rows();
+    goods_list_jump_function();
 }
-function shopping_check(){
+
+function shopping_cart_null_check(){
     if (sessionStorage.getItem("num") == null) { sessionStorage.setItem("num", 0);}//判断开始是否有数.
     var number = sessionStorage.getItem("num");
     document.getElementById("shopping_cart_num").innerText = number;
 }
-function count(){
+
+function shopping_cart_count(){
     var replace = document.getElementById("shopping_cart_num").innerText;//取出id="shopping_cart"标签之间的数，给replace.
     replace = parseInt(replace) + 1;
     sessionStorage.setItem("num", replace);//把数据存人sessionStorage.
     document.getElementById("shopping_cart_num").innerText = replace;//输出数到id="shopping_cart"标签之间。
 }
-function goods_list_jump() {
-    if(document.getElementById("lets_label")){
-        document.getElementById("lets_label").onclick = function () {
-            window.location.href = "goods_list.html";
-        }
-    }
-    if(document.getElementById("index")){
-        document.getElementById("index").onclick = function () {
-            window.location.href = "index.html";
-        }
-    }
-    if(document.getElementById("shopping")){
-        document.getElementById("shopping").onclick = function () {
-            window.location.href = "shopping_cart.html";
-        }
+
+function goods_list_jump_function() {
+    add_button_event("lets_label","goods_list.html");
+    add_button_event("index","index.html");
+    add_button_event("shopping","shopping_cart.html");
+}
+
+function add_button_event(id_name,file_name)
+{
+    document.getElementById(id_name).onclick = function () {
+        window.location.href = file_name;
     }
 }
 
-
+function add_rows() {
+    var goods_info = [
+        {type: '饮料', name: '可口可乐', count: '3', unit: '瓶'},
+        {type: '饮料', name: '雪碧', count: '3', unit: '瓶'},
+        {type: '水果', name: '苹果', count: '5.5', unit: '斤'},
+        {type: '水果', name: '荔枝', count: '15', unit: '斤'},
+        {type: '生活用品', name: '电池', count: '2', unit: '个'},
+        {type: '食品', name: '方便面', count: '4.5', unit: '袋'}];
+    for (var i = 0; i < goods_info.length; i++) {
+        var trObj = document.createElement("tr");//用于创建指定的HTML元素。
+        trObj.id = new Date().getTime();
+        trObj.innerHTML += "<td>" + goods_info[i].type + "</td><td>" + goods_info[i].name + "</td><td>" + goods_info[i].count + "</td><td>" + goods_info[i].unit + "</td><td><button class='button_style' onclick='shopping_cart_count()'>加入购物车</button></td>";
+        document.getElementById("tb").appendChild(trObj);//appendChild()方法向节点添加最后一个子节点.
+    }
+}
--- js/index.js
+++ js/index.js
@@ -2,38 +2,39 @@
  * Created by lenovo on 16-7-7.
  */
 window.onload=function(){
-    shopping_check();
-    count();
-    index_jump();
+    shopping_cart_null_check();
+    display_shopping_cart_num();
+    index_jump_function();
 }
-function shopping_check(){
+function shopping_cart_null_check(){
     if (sessionStorage.getItem("num") == null) { sessionStorage.setItem("num", 0);}//判断开始是否有数.
     var number = sessionStorage.getItem("num");
     document.getElementById("shopping_cart_num").innerText = number;
 }
-function count(){
+function display_shopping_cart_num(){
     var replace = document.getElementById("shopping_cart_num").innerText;//取出id="shopping_cart"标签之间的数，给replace.
     document.getElementById("shopping_cart_num").innerText = replace;//输出数到id="shopping_cart"标签之间。
 }
-function index_jump(){
-    if(document.getElementById("goods_list")){
-        document.getElementById("goods_list").onclick=function(){
-            window.location.href="goods_list.html";
-        }
-    }
-    if(document.getElementById("lets_lable")){
-        document.getElementById("lets_lable").onclick=function(){
-            window.location.href="goods_list.html";
-        }
-    }
-    if(document.getElementById("lets_button")){
-        document.getElementById("lets_button").onclick=function(){
-            window.location.href="goods_list.html";
-        }
-    }
-    if(document.getElementById("shopping")){
-        document.getElementById("shopping").onclick=function(){
-            window.location.href="shopping_cart.html";
-        }
-    }
+function index_jump_function(){
+    add_button_event("lets_label","goods_list.html");
+    add_button_event("goods_list","goods_list.html");
+    add_button_event("lets_button","goods_list.html");
+    add_button_event("shopping","shopping_cart.html");
 }
+function add_button_event(id_name,file_name){
+    document.getElementById(id_name).onclick = function () {
+        window.location.href = file_name;
+    }
+}/*方法一*/
+/*function index_jump_function(){
+    var button_info=[{id:"goods_list",web_link:"goods_list.html"},{id:"lets_label",web_link:"goods_list.html"},{id:"lets_button",web_link:"goods_list.html"},{id:"shopping",web_link:"shopping_cart.html"}]
+    var arr = document.getElementsByTagName('button');
+    for(var i = 0;i<arr.length;i++){
+        arr[i].onclick = function(){
+            for(var j=0;j<button_info.length;j++){
+                if(this.id==button_info[j].id)
+                    document.getElementById(this.id).onclick= window.location.href=button_info[j].web_link;
+            }
+        }
+    }
+} 方法二*/
