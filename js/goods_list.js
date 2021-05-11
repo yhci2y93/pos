c26cda6b73dd6ca00963e7d8905a91ef9cc945e5
yhci2y93 <dhclsy85317375@163.com>
2021-05-11 14:20:32 +0800

    更改了名字

--- js/goods_list.js
+++ js/goods_list.js
@@ -2,47 +2,49 @@
  * Created by lenovo on 16-7-8.
  */
 $(document).ready(function(){
-    check_shopping_cart_null();
-    get_goods_list_jump_function();
-    add_rows();
+    show_shopping_cart_initial();
+    binding_goods_list_jump_function();
+    add_goods_info_rows();
 })
 
-function check_shopping_cart_null(){
-    if (localStorage.getItem("num") == null) { localStorage.setItem("num", 0);}//判断开始是否有数.
-    var number = localStorage.getItem("num");
+function show_shopping_cart_initial(){
+    var number = localStorage.getItem("num")||0;
     $("#shopping_cart_num").text(number);
 }
 
-function shopping_cart_count(){
-    var replace = $("#shopping_cart_num").text();
-    replace = parseInt(replace) + 1;
-    localStorage.setItem("num", replace);
-    $("#shopping_cart_num").text(replace);
+function show_shopping_cart_count(){
+    var number = $("#shopping_cart_num").text();
+    number = parseInt(number) + 1;
+    localStorage.setItem("num", number);
+    $("#shopping_cart_num").text(number);
+
 }
 
-function get_goods_list_jump_function() {
-    add_button_event("lets_label","goods_list.html");
-    add_button_event("index","index.html");
-    add_button_event("shopping","shopping_cart.html");
+function binding_goods_list_jump_function() {
+    add_button_jump_event("lets_label","goods_list.html");
+    add_button_jump_event("index","index.html");
+    add_button_jump_event("shopping","shopping_cart.html");
 }
 
-function add_button_event(id_name,file_name)
+function add_button_jump_event(id_name,file_name)
 {
-    $("button#"+id_name+"").click(function(){
+    $("#"+id_name+"").click(function(){
         window.location.href = file_name;
     });
 }
 
-function add_rows() {
+function add_goods_info_rows() {
     var goods_info = [
-        {type: '饮料', name: '可口可乐', count: '3', unit: '瓶'},
-        {type: '饮料', name: '雪碧', count: '3', unit: '瓶'},
-        {type: '水果', name: '苹果', count: '5.5', unit: '斤'},
-        {type: '水果', name: '荔枝', count: '15', unit: '斤'},
-        {type: '生活用品', name: '电池', count: '2', unit: '个'},
-        {type: '食品', name: '方便面', count: '4.5', unit: '袋'}];
+        {barcode:"000", type: '饮料', name: '可口可乐', count: '3', unit: '瓶'},
+        {barcode:"001", type: '饮料', name: '雪碧', count: '3', unit: '瓶'},
+        {barcode:"002", type: '水果', name: '苹果', count: '5.5', unit: '斤'},
+        {barcode:"003", type: '水果', name: '荔枝', count: '15', unit: '斤'},
+        {barcode:"004", type: '生活用品', name: '电池', count: '2', unit: '个'},
+        {barcode:"005", type: '食品', name: '方便面', count: '4.5', unit: '袋'}];
     for (var i = 0; i < goods_info.length; i++) {
-        $("tbody#table_body").append("<tr><td>" + goods_info[i].type + "</td><td>" + goods_info[i].name + "</td><td>" + goods_info[i].count + "</td><td>" + goods_info[i].unit + "</td><td><button class='button_style' onclick='shopping_cart_count()'>加入购物车</button></td></tr>");
+        //var i=JSON.stringify(goods_info[i]);
+        //localStorage(goods_info[i].barcode,i);
+        $("tbody#table_body").append("<tr><td>" + goods_info[i].type + "</td><td>" + goods_info[i].name + "</td><td>" + goods_info[i].count + "</td><td>" + goods_info[i].unit + "</td><td><button  class='button_style' onclick='show_shopping_cart_count()' id="+goods_info[i].barcode+">加入购物车</button></td></tr>");
     }
 }
 
--- js/index.js
+++ js/index.js
@@ -2,23 +2,23 @@
  * Created by lenovo on 16-7-8.
  */
 $(document).ready(function(){
-    check_shopping_cart_null();
-    get_index_jump_function();
+    show_shopping_cart_initial();
+    binding_goods_list_jump_function();
 });
-function check_shopping_cart_null(){
-    if (localStorage.getItem("num") == null) { localStorage.setItem("num", 0);}//判断开始是否有数.
-    var number = localStorage.getItem("num");
+function show_shopping_cart_initial(){
+    //if (localStorage.getItem("num") == null) { localStorage.setItem("num", 0);}//判断开始是否有数.
+    var number = localStorage.getItem("num")||0;
     $("#shopping_cart_num").text(number);
 }
 
-function get_index_jump_function(){
-    add_button_event("lets_label","goods_list.html");
-    add_button_event("goods_list","goods_list.html");
-    add_button_event("lets_button","goods_list.html");
-    add_button_event("shopping","shopping_cart.html");
+function binding_goods_list_jump_function(){
+    add_button_jump_event("lets_label","goods_list.html");
+    add_button_jump_event("goods_list","goods_list.html");
+    add_button_jump_event("lets_button","goods_list.html");
+    add_button_jump_event("shopping","shopping_cart.html");
 }
 
-function add_button_event(id_name,file_name) {
+function add_button_jump_event(id_name,file_name) {
     $("button#"+id_name+"").click(function(){
         window.location.href = file_name;
     });
