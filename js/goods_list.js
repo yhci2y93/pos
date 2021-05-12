bb9a4eaed9e656eecaf6d19fb6c83376c0558b7a
yhci2y93 <dhclsy85317375@163.com>
2021-05-12 13:32:22 +0800

    添加了把信息传入localStorage

--- js/goods_list.js
+++ js/goods_list.js
@@ -2,26 +2,48 @@
  * Created by lenovo on 16-7-8.
  */
 $(document).ready(function(){
+    var goods_info = [
+        {barcode:"000", type: '饮料', name: '可口可乐', price: '3', unit: '瓶'},
+        {barcode:"001", type: '饮料', name: '雪碧', price: '3', unit: '瓶'},
+        {barcode:"002", type: '水果', name: '苹果', price: '5.5', unit: '斤'},
+        {barcode:"003", type: '水果', name: '荔枝', price: '15', unit: '斤'},
+        {barcode:"004", type: '生活用品', name: '电池', price: '2', unit: '个'},
+        {barcode:"005", type: '食品', name: '方便面', price: '4.5', unit: '袋'}];
     show_shopping_cart_initial();
-    binding_goods_list_jump_function();
-    add_goods_info_rows();
+    bind_goods_list_jump_function();
+    add_goods_info_rows(goods_info);
+    show_shopping_cart_count(goods_info);
+
 })
 
 function show_shopping_cart_initial(){
     var number = localStorage.getItem("num")||0;
     $("#shopping_cart_num").text(number);
 }
-
-$(document).ready(function(){
-    $(".button_style").click(function(){
-        var number = $("#shopping_cart_num").text();
-        number = parseInt(number) + 1;
-        localStorage.setItem("num", number);
-        $("#shopping_cart_num").text(number);
-    });
-});
-
-function binding_goods_list_jump_function() {
+function show_shopping_cart_count(goods_info){
+    //$(".button_style").click(function(){
+    var barcode=[];
+    for(var i=0;i<goods_info.length;i++){
+        $("#"+goods_info[i].barcode+"").click(function(){
+            var number = $("#shopping_cart_num").text();
+            number = parseInt(number) + 1;
+            localStorage.setItem("num", number);
+            $("#shopping_cart_num").text(number);
+            //if(barcode.indexOf(goods_info[i].barcode)==-1){
+            //    barcode.push(goods_info[i].barcode);
+            //    localStorage.setItem(goods_info[i].barcode,JSON.stringify(goods_info[i]));
+            //}
+            for(var j=0;j<goods_info.length;j++){
+                if(barcode.indexOf(goods_info[j].barcode)==-1){
+                    barcode.push(goods_info[j].barcode);
+                    localStorage.setItem(goods_info[j].barcode,JSON.stringify(goods_info[j]));
+                    break;
+                }
+            }
+        });
+    }
+}
+function bind_goods_list_jump_function() {
     add_button_jump_event("lets_label","goods_list.html");
     add_button_jump_event("index","index.html");
     add_button_jump_event("shopping","shopping_cart.html");
@@ -34,14 +56,7 @@ function add_button_jump_event(id_name,file_name)
     });
 }
 
-function add_goods_info_rows(){
-    var goods_info = [
-        {barcode:"000", type: '饮料', name: '可口可乐', price: '3', unit: '瓶'},
-        {barcode:"001", type: '饮料', name: '雪碧', price: '3', unit: '瓶'},
-        {barcode:"002", type: '水果', name: '苹果', price: '5.5', unit: '斤'},
-        {barcode:"003", type: '水果', name: '荔枝', price: '15', unit: '斤'},
-        {barcode:"004", type: '生活用品', name: '电池', price: '2', unit: '个'},
-        {barcode:"005", type: '食品', name: '方便面', price: '4.5', unit: '袋'}];
+function add_goods_info_rows(goods_info){
     var get_string=$("#get_rows").text();
     for(var i = 0; i < goods_info.length; i++){
         var replace=get_string.replace(/type/,goods_info[i].type)
