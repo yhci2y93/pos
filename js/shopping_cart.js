acb8855fe1de8f1ba9e030054836905fcc6de03d
yhci2y93 <dhclsy85317375@163.com>
2021-05-13 17:52:45 +0800

    实现了数据的显示还有加减功能即shoopping_cart数据的显示

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -4,11 +4,39 @@
 $(document).ready(function(){
     var goods_info=localStorage.getItem("goods_info");
     goods_info=JSON.parse(goods_info);
-    add_goods_info_rows(goods_info);
-    //show_shopping_cart_initial();
+    var goods_count=get_goods_num(goods_info);
+    var item_info=get_goods_info(goods_info,goods_count);
+    add_goods_info_rows(item_info);
+    show_shopping_cart_initial();
     get_goods_list_jump_function();
-    show_goods_number();
+    show_goods_number(item_info);
 })
+
+function get_goods_num(goods_info){
+   var goods_count={};
+    for(var i=0;i<goods_info.length;i++){
+        var key=goods_info[i].barcode;
+        if(key in goods_count){
+            goods_count[key]++;
+        }else{
+            goods_count[key]=1;
+        }
+    }
+    return goods_count;
+}
+
+function get_goods_info(goods_info,goods_count){
+    var item_info=[];
+    var barcode=[];
+    for(var i=0;i<goods_info.length;i++){
+        if(barcode.indexOf(goods_info[i].barcode)==-1){
+            barcode.push(goods_info[i].barcode);
+            goods_info[i].count=goods_count[goods_info[i].barcode];
+            item_info.push(goods_info[i])
+        }
+    }
+    return item_info;
+}
 
 function add_goods_info_rows(goods_info){
     var get_string=$("#get_rows").text();
@@ -26,26 +54,21 @@ function add_goods_info_rows(goods_info){
     }
 }
 
-function del_goods_info_row(){
-}
-function get_goods_info(){
-
-
-}
-
-/*function show_shopping_cart_initial(){
-    var barcode=localStorage.getItem("barcode");
-    for(var i=0;i<barcode.length;i++){
-        var number = localStorage.getItem(barcode[i])||0;
-        $("#shopping_cart_num").text(number);
-    }
+/*function del_goods_info_row(){
+    var row_num=$("#table_body").filter("#table_body").size();
+    alert(row_num)
 }*/
 
-function show_goods_number(){
-    var barcode=JSON.parse(localStorage.getItem("barcode"));
+function show_shopping_cart_initial(){
+    var number = localStorage.getItem("num")||0;
+    $("#shopping_cart_num").text(number);
+}
 
+function show_goods_number(item_info){
+    var barcode=JSON.parse(localStorage.getItem("barcode"));
     for(var i=0;i<barcode.length;i++){
-        var number = localStorage.getItem(barcode[i])||1;
+        localStorage.setItem(barcode[i],item_info[i].count);
+        var number = localStorage.getItem(barcode[i])
         $("#"+barcode[i]+"").text(number);
         show_goods_num(barcode[i])
     }
@@ -57,6 +80,8 @@ function show_goods_num(barcode){
         num = parseInt(num) + 1;
         localStorage.setItem(barcode, num);
         $("#"+barcode+"").text(num);
+        var number=parseInt($("#shopping_cart_num").text())+1;
+        $("#shopping_cart_num").text(number);
     });
     $("#dec"+barcode+"").click(function(){
         var num = $("#"+barcode+"").text();
@@ -64,17 +89,14 @@ function show_goods_num(barcode){
         if(num>=0){
             localStorage.setItem(barcode, num);
             $("#"+barcode+"").text(num);
+            var number=parseInt($("#shopping_cart_num").text())-1;
+            $("#shopping_cart_num").text(number);
         }else{
+            //del_goods_info_row();
             //window.location.href = "goods_list.html";
         }
     });
 }
-/*function show_shopping_cart_count(){
-    var replace = $("#shopping_cart_num").text();
-    replace = parseInt(replace) + 1;
-    localStorage.setItem("num", replace);
-    $("#shopping_cart_num").text(replace);
-}*/
 
 function get_goods_list_jump_function() {
     add_button_event("lets_label","goods_list.html");
